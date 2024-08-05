5803b61cd082a47d4422f00494635301abaa57ab
Aadi Kanwar <aadi.kanwar@gmail.com>
2024-08-05 10:48:02 +0530

    First Commit

---
+++ Shortest_Job_First.cpp
@@ -0,0 +1,70 @@
+// Shortest Job First
+
+#include<stdio.h>
+#include<conio.h>
+void findWatiingTime( int n, int bt[], int wt[])
+{
+    wt[0] = 0;
+    for (int i = 1; i < n; i++)
+    {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+}
+
+void findTurnAroundTime( int n, int bt[], int wt[], int tat[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findavgTime(int processes[], int n, int bt[])
+{
+    int wt[n], tat[n];
+    findWatiingTime( n, bt, wt);
+    findTurnAroundTime( n, bt, wt, tat);
+
+    printf("Processes Burst Time Waiting Time Turn-Around Time\n");
+    
+    int total_wt = 0, total_tat = 0;
+    for (int i = 0; i < n; i++)
+    {
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        printf("%d \t\t %d \t\t %d \t\t %d\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+    printf("Average waiting time = %f\n", (float)total_wt / (float)n);
+    printf("Average turn around time = %f\n", (float)total_tat / (float)n);
+}
+
+// Rearrange the processes according to the burst time
+void sjfScheduling(int processes[], int n, int bt[])
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (bt[j] > bt[j + 1])
+            {
+                int temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
+            }
+        }
+    }
+    findavgTime(processes, n, bt);
+}
+
+int main()
+{
+    int processes[] = {1, 2, 3, 4};
+    int n = sizeof(processes) / sizeof(processes[0]);
+    int burst_time[] = {6, 8, 7, 3};
+    sjfScheduling(processes, n, burst_time);
+    return 0;
+}
